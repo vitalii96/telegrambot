cf1627b5998fcf7535ab4e3c2e44a997677d8a71
vitalii96 <panch.vit@gmail.com>
2022-06-14 20:06:36 +0300

    add

--- keyBoard.py
+++ keyBoard.py
@@ -6,20 +6,20 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 bot = telebot.TeleBot(config.TOKEN)
 
 
-class KeyBoard():
+class KeyBoard:
     age = 10
-    def start(self):
-        mess = f'Привіт, {self.from_user.first_name}{self.from_user.last_name}'
+    def start(self,message):
+        mess = f'Привіт, {message.from_user.first_name}{message.from_user.last_name}'
         markup = types.ReplyKeyboardMarkup(
             resize_keyboard=True)  # створюєм екземпляр класу кнопок на клаві та вказуємо, що вони міняють розмір відповідно до кількості
         item1 = types.KeyboardButton('Моя Карта')
         item2 = types.KeyboardButton('Наші магазини')
         item3 = types.KeyboardButton('Гаманець')
         markup.add(item1, item2, item3)
-        bot.send_message(self.chat.id, mess, reply_markup=markup)
+        bot.send_message(message.chat.id, mess, reply_markup=markup)
 
-    def chervonograd_location(self):
-        chat_id = self.chat.id
+    def chervonograd_location(self, message):
+        chat_id = message.chat.id
         markup = types.InlineKeyboardMarkup()
         link1 = types.InlineKeyboardButton(text='Стуса,17', callback_data='STUSA')
         link2 = types.InlineKeyboardButton(text='Бандери,11', callback_data='BANDERY')
